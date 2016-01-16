2c929fd88f322be54e50f7f21ef874c78ca4058e
Kyle <altck8@gmail.com>
2016-01-16 14:54:27 -0800

    refactor c method to be cleaner in variable use

--- word_puzzle.c
+++ word_puzzle.c
@@ -2,17 +2,19 @@
 #include <string.h>
 
 const char*
-RemoveVowels( char* current, char* result )
+RemoveVowels( char* current )
 {
   int i, j;
   char* vowels = "aeiouAEIOU";
-  result = current;
   for( i = 0; current[ i ] != '\0'; ++i )
   {
     for( j = 0; vowels[ j ] != '\0'; ++j )
     {
-      result[ i ] = ( current[ i ] == vowels[ j ] ) ? '-' : current[ i ];
+      if( current[ i ] == vowels[ j ] )
+      {
+        current[ i ] = '-';
+      }
     }
   }
-  return result;
+  return current;
 }
